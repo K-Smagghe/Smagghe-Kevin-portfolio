8e0af12a356bbb57bd7569e14243a20e0a7b1bdf
K-Smagghe <kevin.smagghe.pro@gmail.com>
2026-02-23 21:51:36 +0100

    add-entete-desktop

---
+++ client/src/components/Entete/Entete.tsx
@@ -0,0 +1,32 @@
+import "../Entete/Entete.css";
+
+function Entete() {
+  return (
+    <>
+      <div className="Entete">
+        <h3>
+          <span>JE SUIS BEAUCOUP </span>
+          <span>DE CHOSES !</span>
+        </h3>
+        <div className="Entete-Bloc">
+          <ul>
+            <li># Développeur web</li>
+            <li># Développeur web mobile</li>
+            <li># Fana de la tech</li>
+          </ul>
+          <ul>
+            <li># Chargé de recouvrement amiable</li>
+            <li># Chargé de recouvrement judiciaire</li>
+            <li># Chargé de clientèle</li>
+          </ul>
+          <ul>
+            <li># Curieux de nature</li>
+            <li># Force de proposition</li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Entete;
