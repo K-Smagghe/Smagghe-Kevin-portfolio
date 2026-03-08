0096bbd16c4fa5287582cf74921285cf977cb205
K-Smagghe <kevin.smagghe.pro@gmail.com>
2026-03-08 23:47:38 +0100

    add css form

--- client/src/components/Form/Form.tsx
+++ client/src/components/Form/Form.tsx
@@ -1,12 +1,16 @@
+import "../Form/Form.css";
+
 function Form() {
   return (
     <>
-      <h2>Contact</h2>
-      <p>
-        Vous souhaitez me contacter pour obtenir des informations sur mes
-        services ou mes compétences ? Une question technique ou simplement
-        l'envie d'échanger sur votre futur projet ? C'est par ici !
-      </p>
+      <div className="HautContact">
+        <h2 className="h2Contact">CONTACT</h2>
+        <p className="pContact">
+          Vous souhaitez me contacter pour obtenir des informations sur mes
+          services ou mes compétences ? Une question technique ou simplement
+          l'envie d'échanger sur votre futur projet ? C'est par ici !
+        </p>
+      </div>
       <form action="" method="post">
         <label htmlFor="Nom">Nom</label>
         <input type="text" id="Nom" required />
@@ -18,8 +22,7 @@ function Form() {
         <input type="email" id="Email" required />
         <br />
         <label htmlFor="Message">Votre message</label>
-        <input type="textarea" id="Message" required />
-        <br />
+        <textarea id="Message" required />
         <button type="submit">Envoyer mon message</button>
       </form>
     </>
