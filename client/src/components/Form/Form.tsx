a82b06f6930a7210bb4a0cd9afa9c3a67b88ee07
K-Smagghe <kevin.smagghe.pro@gmail.com>
2026-03-09 16:18:04 +0100

    add back to form

--- client/src/components/Form/Form.tsx
+++ client/src/components/Form/Form.tsx
@@ -1,6 +1,41 @@
+import { useState } from "react";
 import "../Form/Form.css";
 
 function Form() {
+  const [nom, Setnom] = useState("");
+  const [prenom, SetPrenom] = useState("");
+  const [email, SetEmail] = useState("");
+  const [message, SetMessage] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const formData = { nom, prenom, email, message };
+
+    try {
+      const response = await fetch(
+        `${import.meta.env.VITE_API_URL}/api/message`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(formData),
+        },
+      );
+
+      if (response.ok) {
+        alert("Message envoyé !");
+        Setnom("");
+        SetPrenom("");
+        SetEmail("");
+        SetMessage("");
+      }
+    } catch (error) {
+      console.error("Erreur :", error);
+    }
+  };
+
   return (
     <>
       <div className="HautContact">
@@ -11,18 +46,41 @@ function Form() {
           l'envie d'échanger sur votre futur projet ? C'est par ici !
         </p>
       </div>
-      <form action="" method="post">
+      <form action="" method="post" onSubmit={handleSubmit}>
         <label htmlFor="Nom">Nom</label>
-        <input type="text" id="Nom" required />
+        <input
+          type="text"
+          id="Nom"
+          required
+          value={nom}
+          onChange={(e) => Setnom(e.target.value)}
+        />
         <br />
         <label htmlFor="Prenom">Prénom</label>
-        <input type="text" id="Prenom" required />
+        <input
+          type="text"
+          id="Prenom"
+          required
+          value={prenom}
+          onChange={(e) => SetPrenom(e.target.value)}
+        />
         <br />
         <label htmlFor="Email">Email</label>
-        <input type="email" id="Email" required />
+        <input
+          type="email"
+          id="Email"
+          required
+          value={email}
+          onChange={(e) => SetEmail(e.target.value)}
+        />
         <br />
         <label htmlFor="Message">Votre message</label>
-        <textarea id="Message" required />
+        <textarea
+          id="Message"
+          required
+          value={message}
+          onChange={(e) => SetMessage(e.target.value)}
+        />
         <button type="submit">Envoyer mon message</button>
       </form>
     </>
